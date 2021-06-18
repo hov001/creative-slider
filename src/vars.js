48d171a5b7ab8678be67b39649dadd5b934a3441
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-06-18 21:44:40 +0400

    Feat(vars.js): Changed file name

---
+++ src/vars.js
@@ -0,0 +1,6 @@
+// DOM variables
+const btnUp = document.querySelector('.up-button')
+const btnDown = document.querySelector('.down-button')
+const container = document.querySelector('.container')
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
