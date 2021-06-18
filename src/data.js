2f75182dcd3e046e291375fcb6ee29156b5664a7
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-06-18 21:19:30 +0400

    Feat(data.js): Ceated file and added datas

---
+++ src/data.js
@@ -0,0 +1,30 @@
+const data = [
+  {
+    title: 'Automated Customer Service',
+    description: 'Love, death & robots',
+    background: 'linear-gradient(220.16deg, #ffe101 -8%, #f39102 138%)',
+    image:
+      'https://images.unsplash.com/photo-1601574968106-b312ac309953?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1996&q=80',
+  },
+  {
+    title: 'Zima Blue',
+    description: 'Love, death & robots',
+    background: 'linear-gradient(221.87deg, #8308ea 1%, #5305af 128%)',
+    image:
+      'https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2023&q=80',
+  },
+  {
+    title: 'Life Hutch',
+    description: 'Love, death & robots',
+    background: 'linear-gradient(215.32deg, #f90306 -1%, #9e0706 124%)',
+    image:
+      'https://images.unsplash.com/photo-1501529301789-b48c1975542a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+  },
+  {
+    title: 'Snow in the desert',
+    description: 'Love, death & robots',
+    background: 'linear-gradient(229.99deg, #11dee9 -26%, #017e8b 145%)',
+    image:
+      'https://images.unsplash.com/photo-1520263115673-610416f52ab6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80',
+  },
+]
