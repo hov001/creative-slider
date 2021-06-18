c0df2d9b5cecb80206a9ceb6a3190f0ecac875ea
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-06-18 21:50:25 +0400

    Feat(_vars.js): Deleted file

--- src/_vars.js
+++
@@ -1,6 +0,0 @@
-// DOM variables
-const btnUp = document.querySelector('.up-button')
-const btnDown = document.querySelector('.down-button')
-const container = document.querySelector('.container')
-const sidebar = document.querySelector('.sidebar')
-const mainSlide = document.querySelector('.main-slide')
