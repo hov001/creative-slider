cbc4755e5b5f8c4441b67f0038faf93958e84d24
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-06-18 21:22:35 +0400

    Feat(_vars.js): Ceated file and added dom vars

---
+++ src/_vars.js
@@ -0,0 +1,8 @@
+// DOM variables
+const btnUp = document.querySelector('.up-button')
+const btnDown = document.querySelector('.down-button')
+const container = document.querySelector('.container')
+const containerHeight = container.clientHeight
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const slidesCount = mainSlide.querySelectorAll('.main-slide__item').length
