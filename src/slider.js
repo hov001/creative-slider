65192566f69834c6c4c1629e120758ad3a7e4140
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2021-06-18 21:20:01 +0400

    Feat(slider.js): Ceated file and added slider functional

---
+++ src/slider.js
@@ -0,0 +1,45 @@
+// DOM variables
+const btnUp = document.querySelector('.up-button')
+const btnDown = document.querySelector('.down-button')
+const container = document.querySelector('.container')
+const containerHeight = container.clientHeight
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const slidesCount = mainSlide.querySelectorAll('.main-slide__item').length
+
+// Active Index
+let activeSlideIndex = 0
+
+// Last sidebar item
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+
+// Event listeners
+btnUp.addEventListener('click', () => {
+  sliderListener('up')
+})
+btnDown.addEventListener('click', () => {
+  sliderListener('down')
+})
+
+// Slider listener function
+function sliderListener(button) {
+  // Detect button's direction
+  if (button === 'up') {
+    activeSlideIndex++
+    if (activeSlideIndex === slidesCount) {
+      activeSlideIndex = 0
+    }
+  } else if (button === 'down') {
+    activeSlideIndex--
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesCount - 1
+    }
+  }
+
+  mainSlide.style.transform = `translateY(-${
+    containerHeight * activeSlideIndex
+  }px)`
+  sidebar.style.transform = `translateY(${
+    containerHeight * activeSlideIndex
+  }px)`
+}
